0578c868e803a7e358d803106065b351fd59a69d
Eugene Daragan <eugene.daragan@pandadoc.com>
2023-04-29 01:22:34 +0200

    packaging fixes

--- package-shared-sync.js
+++ package-shared-sync.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const sharedPropertiesPath = path.join(__dirname, 'package.shared.json');
+const sharedPropertiesPath = path.join(__dirname, "package.shared.json");
 const sharedProperties = require(sharedPropertiesPath);
 
 function mergeSharedProperties(targetPackagePath) {
-  const packageBasePath = path.join(__dirname, targetPackagePath, 'package.base.json');
-  const packagePath = path.join(__dirname, targetPackagePath, 'package.json');
+  const packageBasePath = path.join(__dirname, targetPackagePath, "package.base.json");
+  const packagePath = path.join(__dirname, targetPackagePath, "package.json");
 
   const packageBaseJson = require(packageBasePath);
 
@@ -16,7 +16,12 @@ function mergeSharedProperties(targetPackagePath) {
   };
 
   fs.writeFileSync(packagePath, JSON.stringify(extendedPackage, null, 2));
+
+  fs.copyFileSync(
+    path.join(__dirname, "README.md"),
+    path.join(__dirname, targetPackagePath, "README.md"),
+  );
 }
 
-mergeSharedProperties('packages/onek');
-mergeSharedProperties('packages/onek-compat');
+mergeSharedProperties("packages/onek");
+mergeSharedProperties("packages/onek-compat");
